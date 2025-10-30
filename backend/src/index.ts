443e7e4ebc6ec67f80234a6e8a187ca120e8bb8a
Prakash Poudel <mee.prakash25@gmail.com>
2025-10-31 05:29:09 +0545

    changed backend/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,24 +19,25 @@ const PORT = process.env.PORT || 8000
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}))
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+)
 
 // app.get("/", (req, res) => {
-  //   res.json({ message: `Backend is running on port: ${PORT}` })
-  // })
-  
+//   res.json({ message: `Backend is running on port: ${PORT}` })
+// })
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
 })
-
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
