e6df9948a94d91076ae7b5eb6a6e7573ab084c61
Prakash Poudel <mee.prakash25@gmail.com>
2025-11-04 23:06:00 +0545

    search hotels e2e test

---
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test"
+
+const UI_URL = "http://localhost:5173/"
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL)
+
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click()
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible()
+
+  await page.locator("[name=email]").fill("1@1.com")
+  await page.locator("[name=password]").fill("password123")
+
+  await page.getByRole("button", { name: "Sign In" }).click()
+
+  await expect(page.getByText("Successfully logged in")).toBeVisible()
+})
+
+test("should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL)
+
+  await page.getByPlaceholder("Where are you going?").fill("Kathmandu")
+  await page.getByRole("button", { name: "Search" }).click()
+
+  await expect(page.getByText("Hotels found in Kathmandu")).toBeVisible()
+  await expect(page.getByText("Kathmandu Gateways").first()).toBeVisible()
+})
